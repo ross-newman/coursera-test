e9d72d03845e8e1609b802525b4ec512f29e2637
Ross Newman <ross.newman@abaco.com>
2018-07-06 10:59:47 +0100

    Added last bug fix

--- course5/module5-solution/src/public/myinfo/myinfo.component.js
+++ course5/module5-solution/src/public/myinfo/myinfo.component.js
@@ -10,16 +10,30 @@ angular.module('public')
   controller: myInfoController
 });
 
-myInfoController.$inject = ['signupInfo'];
-function myInfoController(signupInfo) {
+myInfoController.$inject = ['signupInfo', '$http'];
+function myInfoController(signupInfo, $http) {
   var $ctrl = this;
+  $ctrl.fav = {};
+  $ctrl.fav.valid = false;
   $ctrl.getValid = function () { return signupInfo.user.valid; };
   $ctrl.getFirst = function () { return signupInfo.user.firstname; };
   $ctrl.getLast = function () { return signupInfo.user.lastname; };
   $ctrl.getEmail = function () { return signupInfo.user.email; };
   $ctrl.getPhone = function () { return signupInfo.user.phone; };
   $ctrl.getFavItem = function () { return signupInfo.user.favdish; };
-  $ctrl.test = "arse";
+
+  if ($ctrl.getValid())
+  {
+    $http.get("https://beautiful-wind-cave-15247.herokuapp.com/menu_items/" + signupInfo.user.favdish + ".json").then( function (response) {
+      console.log(response);
+      $ctrl.fav.name = response.data.name;
+      $ctrl.fav.description = response.data.description;
+      $ctrl.fav.name = response.data.name;
+      $ctrl.fav.category_short_name = response.data.category_short_name;
+      $ctrl.fav.valid = true;
+    });
+  
+  }
 };
 
 })();
